49b6c7c6627347031ead8f4d11fbcdcc78b490be
Speedsix Wheels <64212097+speedsixwheels@users.noreply.github.com>
2021-07-14 09:33:37 +0200

    canvi pass

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'speedon_sat' );
 define( 'DB_USER', 'speedon_sat' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Y9B#^dDY1&0N' );
+define( 'DB_PASSWORD', 's@t#sPx0nl1n3&' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
